c7b6acef275249126dc3a83d71510c98d42ab3c0
Snehal979 <bansodsnehal535@gmail.com>
2023-02-16 16:17:04 +0530

    [Snehal]Add: validation of first name

--- UserRegistrationProblemB/Program.cs
+++ UserRegistrationProblemB/Program.cs
@@ -5,6 +5,38 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("User Registration Program");
+            Console.WriteLine("User Registration Program....");
+            Validation code = new Validation();
+            try
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("hint 1.FirstName 2.Exit");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    switch (num)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter the first name");//"Snehal"
+                            string firstName = Console.ReadLine();
+                            Console.WriteLine("FirstName :"+code.ValidationName(firstName));
+                            break;
+                        case 2:
+                            flag = false;
+                            Console.WriteLine("Thank you");
+                            break;
+                    }
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ UserRegistrationProblemB/Validation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblemB
+{
+    public class Validation
+    {
+        const string Name = "^[A-Z]{1}[a-z]{2,}$";
+        /// <summary>
+        /// Uc1 /Uc2Validation First And Last name
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="valueData"></param>
+        /// <returns></returns>
+        public string ValidationName(string input)
+        {
+            if (Regex.IsMatch(input, Name))
+                return $"{input} is Valid";
+            else
+                return $"{input} is Invalid";
+        }
+    }
+}
