84af796aab4a87becde413c59354b482b365a506
Snehal979 <bansodsnehal535@gmail.com>
2023-02-16 16:12:42 +0530

    [Snehal]Add:Welcome Massage For User Registration

---
+++ UserRegistrationProblemB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblemB
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Program");
+        }
+    }
+}
